897e1cac6b75daac08c4cbba17a71e2ea7c12ab6
djwuoo <165735576+djwuoo@users.noreply.github.com>
2024-04-18 12:08:03 -0400

    Add files via upload

---
+++ optativoV.primerparcial.inginf/Repositori/DatabaseConnection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Npgsql;
+
+namespace optativoV.primerparcial.inginf.Repositori
+{
+    public class DatabaseConnection
+    {
+        private string connectionString;
+
+        public DatabaseConnection(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void TestConnection()
+        {
+            try
+            {
+                using (var connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Ejecutar una consulta simple para verificar la conexión
+                    using (var command = new NpgsqlCommand("SELECT 1", connection))
+                    {
+                        command.ExecuteScalar();
+                        Console.WriteLine("La conexión a la base de datos fue exitosa.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al conectar a la base de datos: {ex.Message}");
+            }
+        }
+    }
+}
